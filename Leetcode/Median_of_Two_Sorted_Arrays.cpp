6fe7b1aadebeaa92a05f3770e885c84a4033420c
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2020-04-20 17:57:19 +0530

    Update Median_of_Two_Sorted_Arrays.cpp

--- Leetcode/Median_of_Two_Sorted_Arrays.cpp
+++ Leetcode/Median_of_Two_Sorted_Arrays.cpp
@@ -6,10 +6,6 @@
 #include <queue>
 using namespace std; 
 
-
-
-
-
 class Solution {
 public:
 
@@ -139,143 +135,11 @@ int main() {
     return 0;
 }
 
-
 /*
-
-class Solution {
-public:
-
-	int BinSearch(vector<int>& X, vector<int>& Y, int* start, int* end, int* partitionX, int* partitionY, int* found, int tot_len)
-	{
-		int int_MAX = 2147483647;
-		int int_MIN = -2147483648;
-
-		int maxLeftX = ((*partitionX-1)==0 )? int_MIN : X[*partitionX-1];
-		int minRightX = ((*partitionX)== X.size() )? int_MAX : X[*partitionX];
-
-		int maxLeftY = ((*partitionY-1)==0 )? int_MIN : Y[*partitionY-1];
-		int minRightY = ((*partitionY)== Y.size() )? int_MAX : Y[*partitionY];
-
-		if      ( (maxLeftX<=minRightY) && (maxLeftY<=minRightX) )
-		{
-			*found = 1;
-			return 0;
-		}
-		else if ( (maxLeftX>minRightY) )  // move towards left in X
-		{
-			*start = *partitionX + 1;
-			*partitionX = ((*start)+(*end))/2;
-			*partitionY = (tot_len+1)/2 - *partitionX;
-			return 1;
-		}
-		else // move towards right in X i.e (Y[partitionY-1]>X[partitionX])
-		{
-			*end = *partitionX - 1;
-			*partitionX = (*start+*end)/2;
-			*partitionY = (tot_len+1)/2 - *partitionX;
-			return 2;
-		}
-
-	}
-
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-    	int int_MAX = 2147483647;
-		int int_MIN = -2147483648;
-		int maxLeftX , maxLeftY , minRightY , minRightX;
-        
-        if(nums1.size()>nums2.size())
-        {
-        	return findMedianSortedArrays(nums2, nums1);
-        }
-
-        int len1 = nums1.size();    int len2 = nums2.size();    int tot_len = len1+len2;
-        
-
-        int start = 0;              int end = nums1.size();     int partitionX = (start+end)/2;   int partitionY = (tot_len+1)/2 - partitionX;
-        int found = 0;    
-        int val;
-
-        while(found!=1)
-    	{
-    		val = BinSearch(nums1, nums2, &start, &end, &partitionX, &partitionY, &found, tot_len);
-    	}
-
-        if( tot_len%2 == 0 ) 
-        {
-        	maxLeftX = ((partitionX-1)==0 )? int_MIN : nums1[partitionX-1];
-			minRightX = ((partitionX)== nums1.size() )? int_MAX : nums1[partitionX];
-
-			maxLeftY = ((partitionY-1)==0 )? int_MIN : nums2[partitionY-1];
-			minRightY = ((partitionY)== nums2.size() )? int_MAX : nums2[partitionY];
-
-        	return (double)(max(maxLeftX,maxLeftY)+min(minRightX,minRightY))/2.0;
-        }
-        else
-        {
-        	return (double)max(maxLeftX,maxLeftY);
-        }
-        
-    }
-};
+SAMPLE I/o
+aniruddha@aniruddha-G3-3579:~/MIT/competitions/leetcode$ ./a.out
+[1,3,6,7,90]    
+[2,4,5]
+4.500000*/
 
 
-
-// link : https://www.youtube.com/watch?v=LPFhl65R7ww
-// Do bin search on the sorted arr having smaller length i.e X
-
-
-
-class Solution {
-public:
-
-	int BinSearch(vector<int>& X, vector<int>& Y, int* start, int* end, int* partitionX, int* partitionY, int* found, int tot_len)
-	{
-		if      ( (X[(*partitionX)-1]<=Y[(*partitionY)]) && (Y[(*partitionY)-1]<=X[(*partitionX)]) )
-		{
-			*found = 1;
-			return 0;
-		}
-		else if ( (X[(*partitionX)-1]>Y[(*partitionY)]) )  // move towards left in X
-		{
-			*start = (*partitionX) + 1;
-			*partitionX = ((*start)+(*end))/2;
-			*partitionY = (tot_len+1)/2 - (*partitionX);
-			return 1;
-		}
-		else // move towards right in X i.e (Y[partitionY-1]>X[partitionX])
-		{
-			*end = (*partitionX) - 1;
-			*partitionX = ((*start)+(*end))/2;
-			*partitionY = (tot_len+1)/2 - (*partitionX);
-			return 2;
-		}
-
-	}
-
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-
-        int len1 = nums1.size();    int len2 = nums2.size();    int tot_len = len1+len2;
-        vector<int>& X = (len1<=len2)? nums1 : nums2; 
-        vector<int>& Y = (len1> len2)? nums1 : nums2;
-        int start = 0;              int end = X.size();  int partitionX = (start+end)/2;   int partitionY = (tot_len+1)/2 - partitionX;
-        int found = 0;    
-        int val;
-
-        while(found!=1)
-    	{
-    		val = BinSearch(X, Y, &start, &end, &partitionX, &partitionY, &found, tot_len);
-    	}
-
-        if( tot_len%2 == 0 ) 
-        {
-        	return (double)(max(X[partitionX-1],Y[partitionY-1])+min(X[partitionX],Y[partitionY]))/2.0;
-        }
-        else
-        {
-        	return (double)max(X[partitionX-1],Y[partitionY-1]);
-        }
-        
-    }
-};
-
-*/
