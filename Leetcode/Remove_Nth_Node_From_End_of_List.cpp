5122d9bbfd36e7f4437344fe4b047cb55b81b7dc
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2020-04-23 07:32:38 +0530

    Add files via upload

---
+++ Leetcode/Remove_Nth_Node_From_End_of_List.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+#include <cstdlib>
+#include <queue>
+using namespace std; 
+
+
+
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+ 
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+
+        if(n==1 && head->next==NULL)
+            return NULL;
+        
+        ListNode* first = head;
+        ListNode* second = head;
+        int count = 0;
+        while(count!=n+1 && first!=NULL)
+        {
+            first = first->next;
+            ++count;
+        }
+        if(count!=n+1)
+        {
+            if(n>1)
+                return head->next;
+            else if(n==1)
+            {
+                while(second->next->next!=NULL )
+                    second=second->next;
+                second->next=NULL;
+                return head;
+            }
+        }
+        
+        while(first!=NULL)
+        {
+            second = second->next;
+            first = first->next;
+        }
+        
+        ListNode* cur = second->next->next;
+        second->next = cur;
+        
+        return head;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+ListNode* stringToListNode(string input) {
+    // Generate list from the input
+    vector<int> list = stringToIntegerVector(input);
+
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+string listNodeToString(ListNode* node) {
+    if (node == nullptr) {
+        return "[]";
+    }
+
+    string result;
+    while (node) {
+        result += to_string(node->val) + ", ";
+        node = node->next;
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        ListNode* head = stringToListNode(line);
+        getline(cin, line);
+        int n = stringToInteger(line);
+        
+        ListNode* ret = Solution().removeNthFromEnd(head, n);
+
+        string out = listNodeToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
