2a8251010f2b0a165a6986524b7c39bde01d91e4
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2020-05-19 07:26:33 +0530

    optimised

--- lab6/mergeSort.cpp
+++ lab6/mergeSort.cpp
@@ -45,7 +45,7 @@ public:
     }
 };
 
-//FASTER CODE
+//FASTER CODE 56ms
 
 class Solution {
 public:
@@ -82,5 +82,55 @@ public:
     vector<int> sortArray(vector<int>& nums) {
         mergeSort(nums,0,nums.size()-1);
         return nums;
+    }
+};
+
+// More FASTER  CODE 48ms
+class Solution {
+public:
+    
+    void merge(int nums[], int left , int mid, int right) {
+        int p = left ,q = mid+1,k = 0;
+        int Arr[right-left+1];
+        for(int i = left ; i<=right; ++i) {
+            if(p > mid ) 
+                Arr[k++] = nums[q++]; 
+            else if(q > right)
+                Arr[k++] = nums[p++];
+            else if( nums[ p ] < nums[ q ])     
+                Arr[k++] = nums[p++];
+            else
+                Arr[k++] = nums[q++];
+        }
+        for (int p=0 ; p< k ;++p) {
+             nums[ left++ ] = Arr[ p ] ;                          
+        }
+    }
+    
+    void mergeSort(int nums[], int left , int right) {
+        int mid;
+        if (left < right) 
+        { 
+            mid = left+(right-left)/2; 
+            mergeSort(nums, left, mid); 
+            mergeSort(nums, mid+1, right); 
+            merge(nums, left, mid, right); 
+        } 
+    }
+    
+    vector<int> sortArray(vector<int>& num) {
+        int end = num.size();
+        int nums[end];
+        for(int i=0 ;i <end;++i)
+        {
+            nums[i] = num[i];
+        }
+        mergeSort(nums,0,end-1);
+        for(int i=0 ;i <end;++i)
+        {
+            num[i] = nums[i];
+        }
+        
+        return num;
     }
 };
