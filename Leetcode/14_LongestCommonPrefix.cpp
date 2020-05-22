2f2d387c3e4bef3f5abf7b1ccb75759428b32b37
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2020-05-22 21:41:41 +0530

    Bin Search

---
+++ Leetcode/14_LongestCommonPrefix.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int min(int x, int y) {
+        return x<y?x:y;
+    }
+    
+    bool isCommonPrefix(vector<string>& strs, int len) {
+        string str1 = strs[0].substr(0, len );
+        for(int i=1; i<strs.size(); ++i) {
+            if(strs[i].find(str1)!=0) 
+                return false;
+        }
+        return true;
+    }
+    
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size() == 0)
+            return "";
+        int minLen = 2147483647;
+        for (int i=0; i<strs.size(); ++i) {
+            minLen = min(minLen, strs[i].length());
+        }
+        int low = 1;
+        int high = minLen;
+        while (low <= high) {
+            int middle = (low + high) / 2;
+            if (isCommonPrefix(strs, middle))
+                low = middle + 1;
+            else
+                high = middle - 1;
+        }
+        return strs[0].substr(0, (low + high) / 2);
+    }
+};
