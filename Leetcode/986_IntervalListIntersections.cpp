2b08b90335ff9c6728fae397ba495ba8d5766f6c
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2020-06-08 23:15:37 +0530

    Add files via upload

---
+++ Leetcode/986_IntervalListIntersections.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
+        vector<vector<int>> ans;
+        int i,j;
+        for (i=0 , j=0;i<A.size() && j<B.size(); ) {
+            int lo = max(A[i][0],B[j][0]);
+            int hi = min(A[i][1],B[j][1]);
+            if(lo <= hi) {
+                ans.push_back({lo,hi});
+            }
+            if (A[i][1]<B[j][1]) 
+                ++i;
+            else
+                ++j;
+        }
+        
+        return ans;
+    }
+};
