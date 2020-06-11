3d7d8cdcd8f0d7e7689da0c0c140c205fa9a8d96
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2020-06-11 20:02:36 +0530

    Dynamic Prog

--- Leetcode/139_WordBreak.cpp
+++ Leetcode/139_WordBreak.cpp
@@ -1,3 +1,37 @@
+//link :https://leetcode.com/problems/word-break/discuss/43814/C%2B%2B-Dynamic-Programming-simple-and-fast-solution-(4ms)-with-optimization
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        if(wordDict.size()==0) return false;
+        
+        set<string> dict;
+        for(auto w: wordDict)
+            dict.insert(w);
+        
+        vector<bool> dp(s.size()+1,false);
+        dp[0]=true;
+        
+        for(int i=1;i<=s.size();i++)
+        {
+            for(int j=i-1;j>=0;j--)
+            {
+                if(dp[j])
+                {
+                    string word = s.substr(j,i-j);
+                    if(dict.find(word)!= dict.end())
+                    {
+                        dp[i]=true;
+                        break; //next i
+                    }
+                }
+            }
+        }
+        
+        return dp[s.size()];
+    }
+};
+
+
 // My approach 
 /* fails for input:
 "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"
