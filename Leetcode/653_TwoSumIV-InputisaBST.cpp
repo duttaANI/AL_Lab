cb35d60109e8a7df4c5d20ba60a06e11735b1505
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2020-06-15 19:12:11 +0530

    Recur DFS Set

---
+++ Leetcode/653_TwoSumIV-InputisaBST.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public: 
+    set<int> s;
+    
+    bool findTarget(TreeNode* root, int k) {
+        if(root==NULL) return 0;
+        if(s.find(root->val)!=s.end())
+        { 
+            return 1;
+        }
+        else
+            s.insert(k-root->val);
+        
+        return findTarget(root->left,k) || findTarget(root->right,k);
+    }
+};
