68007e863abd783e9af71c0dc4f3c2dce702d6e8
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2020-06-17 18:55:55 +0530

    Math

---
+++ Leetcode/628_MaximumProductofThreeNumbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        int min1 = INT_MAX, min2 = INT_MAX;
+        int max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MIN;
+        for(int n :nums) {
+            if(n <= min1) {
+                min2 = min1;
+                min1 = n;
+            } else if ( n <= min2) {
+                min2 = n;
+            }
+            if ( n >= max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if ( n>= max2 ) {
+                max3 = max2;
+                max2 =n;
+            } else if ( n>= max3) {
+                max3 = n;
+            }
+        }
+        return max(min1*min2*max1, max1*max2*max3);
+    }
+};
