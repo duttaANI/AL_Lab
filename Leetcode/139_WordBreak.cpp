7b07e5e917c27f2b67047db82e401e481ddccef2
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2020-06-23 18:49:36 +0530

    logic

--- Leetcode/139_WordBreak.cpp
+++ Leetcode/139_WordBreak.cpp
@@ -1,4 +1,45 @@
+// faster than 100%
+class Solution {
+public:
+    bool wordBreak(string A, vector<string>& B) {
+        int N=A.size();
+        int n=B.size();
+
+        vector<bool> dp(N+1,0);
+        dp[0]=1;
+        for(int i=0;i<N;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                int idx1=i;
+                int idx2=B[j].size()-1;
+
+                while(idx1>=0 && idx2>=0 && A[idx1]==B[j][idx2])
+                    idx1--, idx2--;
+
+                if(idx2==-1)
+                    dp[i+1]=dp[idx1+1];
+                if(dp[i+1])
+                    break;
+            }
+        }
+
+        return dp[N];
+    }
+
+};
+
+
+
+
+
+
+
+
 //link :https://leetcode.com/problems/word-break/discuss/43814/C%2B%2B-Dynamic-Programming-simple-and-fast-solution-(4ms)-with-optimization
+
+
+
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
