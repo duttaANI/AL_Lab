6a83160d1eccaed764df22bcac05293c1c1977ef
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2021-02-24 16:32:35 +0530

    Similar to 862

--- Leetcode/209_MinimumSizeSubarraySum.cpp
+++ Leetcode/209_MinimumSizeSubarraySum.cpp
@@ -1,4 +1,30 @@
-
+// similar to : https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        int n = nums.size(); 
+        if(n==0) return 0;
+        int ans = n+1;
+        deque<int> dq;
+        for(int i=0;i<n;++i){
+            if(i!=0){
+                nums[i]+=nums[i-1];
+            }
+            
+            if(nums[i]>=s){
+                ans = min({ans, i+1 });
+            }
+            
+            while( dq.size() && nums[i]-nums[dq.front()]>=s ){
+                ans = min({ans, i-dq.front() });
+                dq.pop_front();
+            }
+            
+            dq.push_back(i);
+        }
+        return (ans!=n+1)?ans:0;
+    }
+};
 
 
 ///////////////////////////////
