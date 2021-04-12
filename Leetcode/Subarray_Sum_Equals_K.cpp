61d44a6a087da3f166d044a531395d88ea6a0708
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2021-04-12 16:30:04 +0530

    Map

--- Leetcode/Subarray_Sum_Equals_K.cpp
+++ Leetcode/Subarray_Sum_Equals_K.cpp
@@ -1,3 +1,25 @@
+// my
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size(),ans=0;
+        unordered_map<int,int> mp;
+        int sum = 0;
+        mp[sum] = 1;
+        for( int i=0;i<n;++i ){
+            sum += nums[i];
+            
+            // cout << mp[sum] << " " << mp[sum-k] << "\n";
+            if( mp.find(sum-k)!=mp.end()  ){
+                ans+=mp[sum-k];
+            }
+            mp[sum]++;
+        }
+        return ans;
+    }
+};
+
 // link: https://leetcode.com/problems/subarray-sum-equals-k/solution/
 
 
