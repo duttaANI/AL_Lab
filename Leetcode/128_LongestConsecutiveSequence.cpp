258111c789a4ee3bb42cc5741ee4dd938111170b
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2021-04-18 14:43:27 +0530

    Union Find , DSU or set

--- Leetcode/128_LongestConsecutiveSequence.cpp
+++ Leetcode/128_LongestConsecutiveSequence.cpp
@@ -1,3 +1,53 @@
+// union find
+
+class Solution {
+public:
+    
+    unordered_map<int,int> mp;
+    static const int mxN = 1e5+5;
+    int p[mxN],size[mxN];
+    
+    int find(int x){
+        return p[x]^x?p[x]=find(p[x]):x;
+    }
+    
+    void join(int x, int y){
+        int a,b;
+        if( (a=find(x)) == (b=find(y)) ){
+            return ;
+        }
+        if( size[a]>size[b] ) swap(a,b);
+        p[a] = b;
+        size[b] += size[a];
+        return ;
+    }
+    
+    int longestConsecutive(vector<int>& nums) {
+        int i,j, n = nums.size();
+        for(int i=0;i<100005;++i){
+            p[i]=i;
+            size[i]=1;
+        }
+        for(i=0;i<n;++i){
+            if(mp.count(nums[i]) ) continue;
+            if( mp.count(nums[i]-1) ){
+                join( mp[nums[i]-1], i );
+            }
+            if( mp.count(nums[i]+1) ){
+                join( mp[nums[i]+1], i );
+            }
+            mp[nums[i]] = i;
+        }
+        int ans = 0;
+        for(i=0;i<n;i++){
+            ans=max(ans,size[i]);
+        }
+        return ans;
+        
+    }
+};
+
+//set
 class Solution {
 public:
     int longestConsecutive(vector<int>& num) {
