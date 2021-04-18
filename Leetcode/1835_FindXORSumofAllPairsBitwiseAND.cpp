d929751c8364d0d4ce86aae7b82e5ecb6d85856a
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2021-04-18 20:52:13 +0530

    Distributive property is similar for AND and XOR here. (a1^a2) & (b1^b2) = (a1&b1) ^ (a1&b2) ^ (a2&b1) ^ (a2&b2)

--- Leetcode/1835_FindXORSumofAllPairsBitwiseAND.cpp
+++ Leetcode/1835_FindXORSumofAllPairsBitwiseAND.cpp
@@ -1,3 +1,5 @@
+// Distributive property is similar for AND and XOR here.
+// (a1^a2) & (b1^b2) = (a1&b1) ^ (a1&b2) ^ (a2&b1) ^ (a2&b2)
 class Solution {
 public:
     int getXORSum(vector<int>& A, vector<int>& B) {
