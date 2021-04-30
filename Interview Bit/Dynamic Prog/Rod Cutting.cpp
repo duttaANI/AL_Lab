500a2e9b99b3410ab00aea4c2a7585d812bc59a7
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2021-04-30 13:59:33 +0530

    Unbounded knapsack

--- Interview Bit/Dynamic Prog/Rod Cutting.cpp
+++ Interview Bit/Dynamic Prog/Rod Cutting.cpp
@@ -39,3 +39,87 @@ vector<int> Solution::rodCut(int A, vector<int> &B) {
     
 }
 
+// Nayan -> 
+
+vector<int> res;
+int calculate(int i,int j,int l ,int r,vector<vector<int>>& dp,vector<int>& cuts){
+    if(i<0||i>j||j>=cuts.size())return 0;
+    if(i==j)return r-l;
+    if(dp[i][j]!=-1)return dp[i][j];
+    int ans=INT_MAX,prev;
+    for(int k=i;k<=j;k++){
+        prev=ans;
+        ans=min(ans,(r-l)+calculate(i,k-1,l,cuts[k],dp,cuts)+calculate(k+1,j,cuts[k],r,dp,cuts));
+        if(ans<prev){
+            if(res.size())res.pop_back();
+            res.push_back(cuts[k]);
+        }
+    }
+    dp[i][j]=ans;
+    return dp[i][j];
+}
+
+
+vector<int> Solution::rodCut(int A, vector<int> &B) {
+    res.clear();
+    sort(B.begin(),B.end());
+    vector<vector<int>> dp(B.size(),vector<int>(B.size(),-1));
+    int mincost = calculate(0,B.size()-1,0,A,dp,B);
+    // for(auto a : dp){
+    //     for(auto b : a){
+    //         cout<<b<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    // cout<<mincost<<endl;
+    return res;
+}
+
+// my below doesnt work
+
+// unordered_map<int,vector<int>> costPath;
+// unordered_map<string,int> mincost; // min cost from i to j cuts
+// vector<int> C;
+// int solve(int l,int r, vector<int>& path, int  cost){
+//     if(l<r){
+//         cout << C[l] << " " << C[r] << endl;
+//         string str = to_string(l) + " " + to_string(r);
+//         if( mincost.find(str)!=mincost.end() ){
+//             return mincost[str];
+//         }
+        
+//         int mini = INT_MAX;
+//         for(int k=l+1;k<r;++k){
+//             path.push_back(C[k]);
+//             mini =   min({ mini , cost + solve(l,k,path,cost+(C[k]-C[l])) , cost + solve(k,r,path,cost+(C[r]-C[k])) });
+//             path.pop_back();
+//         }
+//         return mincost[str] = mini;
+//     }
+//     else{
+//         cout << " PAth: ";
+//         for(int i=0;i<path.size();++i){
+//             cout << path[i] << " ";
+//         }
+//         cout << endl;
+//         if(costPath.find(cost)==costPath.end())
+//         costPath[cost] = path;
+//     }
+// }
+
+// vector<int> Solution::rodCut(int A, vector<int> &B) {
+//     sort(B.begin(),B.end());
+//     C.clear();
+//     C.push_back(0);
+//     for(int i=0;i<B.size();++i){
+//         C.push_back(B[i]);
+//     }
+//     C.push_back(A);
+//     vector<int> path;
+//     int cost =A;
+//     int r = C.size()-1;
+//     int mini = solve(0,r,path,cost);
+    
+//     return costPath[mini];
+// }
+
