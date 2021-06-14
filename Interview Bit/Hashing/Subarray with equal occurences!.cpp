1f89e980c3bd67987bd5dc5746bfc0f98832ee3e
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2021-06-14 18:21:22 +0530

    Map

--- Interview Bit/Hashing/Subarray with equal occurences!.cpp
+++ Interview Bit/Hashing/Subarray with equal occurences!.cpp
@@ -1,13 +1,25 @@
-int Solution::solve(vector<int> &a, int b, int c) {
+int Solution::solve(vector<int> &A, int B, int C) {
+    // Add 1 if it is B 
+    // Sub 1 if it is C
+    // Else do nothing
+    
     int sum=0,ans=0;
-    unordered_map<int,int> m;
-    m[0] = 1;
-    for(int i : a)
-    {
-        if(i == b) sum += 1;
-        else if(i == c) sum += -1;
-        ans += m[sum];
-        m[sum]++;
+    
+    unordered_map<int,int> seen;
+    seen[0]=1;
+    
+    for(int i=0;i<A.size();i++){
+        
+        if(A[i]==B){
+            sum++;
+        }else if(A[i]==C){
+            sum--;
+        }
+        
+        if(seen.find(sum)!=seen.end()){
+            ans += seen[sum];
+        }
+        seen[sum]++;
     }
     return ans;
 }
