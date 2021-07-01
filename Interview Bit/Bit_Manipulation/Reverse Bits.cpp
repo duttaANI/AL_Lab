34182e540bc3c5231885744c4e11393187bea00b
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2021-07-01 14:57:43 +0530

    left shift , right shift

---
+++ Interview Bit/Bit_Manipulation/Reverse Bits.cpp
@@ -0,0 +1,9 @@
+unsigned int Solution::reverse(unsigned int a) {
+    unsigned int b = 0;
+    for(int i=0;i<32;++i){
+        b = b<<1;
+        b = b | (a&1);
+        a = a>>1;
+    }
+    return b;
+}
