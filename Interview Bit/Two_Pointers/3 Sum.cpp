f525c773ff412c017d3ae5d4f4d6f7fd2687e767
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2021-07-10 05:08:11 +0530

    two pointer without bin search

---
+++ Interview Bit/Two_Pointers/3 Sum.cpp
@@ -0,0 +1,24 @@
+#define ll long long
+
+int Solution::threeSumClosest(vector<int> &nums, int target) {
+    ll result = nums[0] + nums[1] + nums[2];
+    sort(nums.begin(),nums.end());
+    ll left_ptr,right_ptr,cur_sum;
+    for(int i=0; i< nums.size(); ++i)
+    {
+        left_ptr = i+1;
+        right_ptr = nums.size()-1;
+        while(left_ptr<right_ptr)
+        {
+            cur_sum = nums[i] + nums[left_ptr] + nums[right_ptr];
+            if(cur_sum > target) right_ptr--;
+            else ++left_ptr;
+            
+            if(abs(cur_sum - target)< abs(result-target))
+                result = cur_sum;
+        }
+    }
+    
+    return result;
+}
+
