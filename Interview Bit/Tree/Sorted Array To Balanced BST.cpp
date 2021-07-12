ad7ec64b989fcf7dfb3b905237ca2dd32fbf8e66
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2021-07-12 19:47:15 +0530

    recur

--- Interview Bit/Tree/Sorted Array To Balanced BST.cpp
+++ Interview Bit/Tree/Sorted Array To Balanced BST.cpp
@@ -1,23 +1,15 @@
-/**
- * Definition for binary tree
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-TreeNode *res(const vector<int> &vec, int start, int end){
-    if(start > end) return nullptr;
-    if(start == end) return new TreeNode(vec[start]);
-    int mid = start + (end-start)/2;
-    TreeNode *node = new TreeNode(vec[mid]);
-    node->left = res(vec, start, mid-1);
-    node->right = res(vec, mid+1, end);
-    return node;
+TreeNode* solve(int l , int r, const vector<int> &A){
+    if( l<=r ){
+        int mid = l + (r-l)/2;
+        TreeNode * T = new TreeNode(A[mid]);
+        T -> left = solve(l,mid-1,A);
+        T ->right = solve(mid+1,r,A);
+        return T;
+    }
+    return NULL;
 }
-
 TreeNode* Solution::sortedArrayToBST(const vector<int> &A) {
-    return res(A, 0, A.size()-1);
+    int n = A.size();
+    if(n==0) return NULL;
+    return solve(0,n-1,A);
 }
-
