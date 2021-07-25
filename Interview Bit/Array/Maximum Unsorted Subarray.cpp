7de90318b3ea5a713d4b8ab61a29b0769d2921ae
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2021-07-25 16:58:29 +0530

    find min and max elements index

---
+++ Interview Bit/Array/Maximum Unsorted Subarray.cpp
@@ -0,0 +1,13 @@
+vector<int> Solution::subUnsort(vector<int> &A) {
+    int n=A.size(),l=n,r=0,mx=INT_MIN,mn=INT_MAX;
+    for(int i=0;i<n;i++){
+        if(A[i]<mx)r=i;
+        mx=max(A[i],mx);
+    }
+    for(int i=n-1;i>=0;i--){
+        if(A[i]>mn)l=i;
+        mn=min(mn,A[i]);
+    }
+    if(l==n)return {-1};
+    return {l,r};
+}
