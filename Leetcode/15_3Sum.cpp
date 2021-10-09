49042680785a6903af54af7b0e616693372cc2bd
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2021-10-09 16:14:43 +0530

    Two pointer

--- Leetcode/15_3Sum.cpp
+++ Leetcode/15_3Sum.cpp
@@ -1,3 +1,49 @@
+#include <bits/stdc++.h>
+#define ar array
+
+class Solution {
+public:
+    
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        
+        if (nums.size() < 3) {
+            return res;
+        }
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i <= nums.size()-3; i++) {
+            int start = i+1;
+            int end = nums.size()-1;
+            int twosum = 0 - nums[i]; // NOTE ZERO CAN BE MODIFIED FOR SCALING HERE
+            if( i>0 && nums[i]==nums[i-1] ){
+                continue;
+            }
+            while(start<end){
+                int target = nums[start] + nums[end];
+                if( target == twosum ){
+                    res.push_back({nums[i], nums[start], nums[end]});
+                    start++;
+                    end--;
+                    while (start < nums.size() && nums[start] == nums[start-1]) {
+                        start++;
+                    }
+                    while (end >= 0 && nums[end+1] == nums[end]) {
+                        end--;
+                    }
+                }
+                else if (  target < twosum ){
+                    ++start;
+                }
+                else{
+                    --end;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+
 class Solution {
 public:
     
