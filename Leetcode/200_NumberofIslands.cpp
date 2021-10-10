fa3c7e0ed26036361c3aad5af45858accc3f3732
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2021-10-10 20:22:27 +0530

    DFS

--- Leetcode/200_NumberofIslands.cpp
+++ Leetcode/200_NumberofIslands.cpp
@@ -1,3 +1,52 @@
+class Solution {
+public:
+    
+    int dX[4] ={0,1,0,-1};
+    int dY[4] ={1,0,-1,0};
+    
+    int H;
+    int W;
+    
+    bool valid(int x, int y){
+        if( x>=0 && y>=0 && x<H && y<W  ){
+            return 1;
+        }
+        return 0;
+    }
+    
+    void dfs(int x,int y,vector<vector<char>>& grid, vector<vector<int>>& vis ){
+        vis[x][y] = 1;
+        for(int i=0;i<4;++i){
+            int nx = x + dX[i];
+            int ny = y + dY[i];
+            if( valid(nx,ny) && grid[nx][ny]=='1' && !vis[nx][ny]  ){
+                dfs(nx,ny,grid,vis);
+            }
+        }
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        int ans = 0;
+        
+        H = grid.size();
+        W = grid[0].size();
+        
+        vector<vector<int>> vis( H , vector<int> (W,0) );
+        
+        for(int i=0;i<H;++i){
+            for(int j=0;j<W;++j){
+                if(!vis[i][j] && grid[i][j]=='1'  ){
+                    ++ans;
+                    dfs(i,j,grid,vis);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
 #define fastIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 class Solution {
 public:
