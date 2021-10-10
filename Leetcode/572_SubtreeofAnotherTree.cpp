ad426e98144c8e594885db5511b78e4760a09155
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2021-10-10 23:17:54 +0530

    DFS

--- Leetcode/572_SubtreeofAnotherTree.cpp
+++ Leetcode/572_SubtreeofAnotherTree.cpp
@@ -1,3 +1,46 @@
+// My approach
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    bool isSame( TreeNode*a, TreeNode* b ){
+        if(!a && !b) return 1;
+        if(  (a && !b) || (!a && b) ) return 0;
+        if( a->val == b->val  ){
+            return isSame(a->left,b->left) && isSame(a->right,b->right );
+        }
+        else{
+            return 0;
+        }
+    }
+    
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if( isSame(root,subRoot) ){
+            return 1;
+        }
+        else if( root && subRoot ){
+            return isSubtree( root->left, subRoot  ) || isSubtree( root->right, subRoot  );
+        }
+        else{
+            return 0;
+        }
+        
+    }
+};
+
+
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
