7c74f85c927532bf3837ebcd751ddcf279201b4c
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2022-01-31 16:59:33 +0530

    O(n)

---
+++ Leetcode/2149_RearrangeArrayElementsbySign.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<int> ans(n,0);
+        int j = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                ans[j] = nums[i];
+                j+=2;
+            }
+        }
+        j = 1;
+        for(int i=0;i<n;++i){
+            if(nums[i]<0){
+                ans[j] = nums[i];
+                j+=2;
+            }
+        }
+        
+        return ans;
+    }
+};
