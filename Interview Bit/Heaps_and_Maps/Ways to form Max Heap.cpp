88a210cae15530665ddea014dc15cbdcd54830cd
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2022-04-10 22:42:58 +0530

    DP

--- Interview Bit/Heaps_and_Maps/Ways to form Max Heap.cpp
+++ Interview Bit/Heaps_and_Maps/Ways to form Max Heap.cpp
@@ -1,3 +1,105 @@
+#include <iostream>
+using namespace std;
+
+const int mod = 1e9 +7;
+ 
+#define MAXN 105 // maximum value of n here
+ 
+// dp[i] = number of max heaps for i distinct integers
+long dp[MAXN];
+ 
+// nck[i][j] = number of ways to choose j elements
+//             form i elements, no order */
+long nck[MAXN][MAXN];
+ 
+// log2[i] = floor of logarithm of base 2 of i
+int log_2[MAXN];
+ 
+// to calculate nCk
+long choose(int n, int k)
+{
+    if (k > n)
+        return 0;
+    if (n <= 1)
+        return 1;
+    if (k == 0)
+        return 1;
+ 
+    if (nck[n][k] != -1)
+        return nck[n][k];
+ 
+    long answer = (choose(n - 1, k - 1) + choose(n - 1, k))%mod;
+    nck[n][k] = answer;
+    return answer;
+}
+ 
+// calculate l for give value of n
+int getLeft(int n)
+{
+    if (n == 1)
+        return 0;
+ 
+    int h = log_2[n];
+ 
+    // max number of elements that can be present in the
+    // hth level of any heap
+    int numh = (1 << h); //(2 ^ h)
+ 
+    // number of elements that are actually present in
+    // last level(hth level)
+    // (2^h - 1)
+    int last = n - ((1 << h) - 1);
+ 
+    // if more than half-filled
+    if (last >= (numh / 2))
+        return (1 << h) - 1; // (2^h) - 1
+    else
+        return (1 << h) - 1 - ((numh / 2) - last);
+}
+ 
+// find maximum number of heaps for n
+int numberOfHeaps(int n)
+{
+    if (n <= 1)
+        return 1;
+ 
+    if (dp[n] != -1)
+        return dp[n];
+ 
+    int left = getLeft(n)%mod;
+    int ans = ((choose(n - 1, left)%mod * numberOfHeaps(left)%mod)%mod *
+                             (numberOfHeaps(n - 1 - left))%mod)%mod;
+    dp[n] = ans;
+    return ans;
+}
+
+
+int Solution::solve(int n) {
+    for (int i = 0; i <= n; i++)
+        dp[i] = -1;
+ 
+    for (int i = 0; i <= n; i++)
+        for (int j = 0; j <= n; j++)
+            nck[i][j] = -1;
+ 
+    int currLog2 = -1;
+    int currPower2 = 1;
+ 
+    // for each power of two find logarithm
+    for (int i = 1; i <= n; i++) {
+        if (currPower2 == i) {
+            currLog2++;
+            currPower2 *= 2;
+        }
+        log_2[i] = currLog2;
+    }
+ 
+    return numberOfHeaps(n);
+}
+
+
+
+
 #define MAXN 105
 
 long long int dp[MAXN];  	/* dp[i] = number of max heaps for i distinct integers */
